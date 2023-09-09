e310b288941825c7128cd0d947665b0b50caeebb
edg77777 <2394310671@qq.com>
2023-09-09 14:44:00 +0800

    git test first commit

---
+++ src/main/java/com/fzj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.fzj.git;
+
+/**
+ * @author fzj
+ * @date 2023-09-09 14:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
