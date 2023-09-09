6f9fc72f7a489034baf94bfd437415d818fbf834
Froengen <110658182+edg7777@users.noreply.github.com>
2023-09-09 15:06:00 +0800

    Update GitTest.java

--- src/main/java/com/fzj/git/GitTest.java
+++ src/main/java/com/fzj/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git again!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
